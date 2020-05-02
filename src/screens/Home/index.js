b43b73cac960f90de2355c23d95448963dd2fcfe
mathias.lima <mathias.morais7@gmail.com>
2020-05-01 21:30:02 -0300

    update

---
+++ src/screens/Home/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { MaterialCommunityIcons, AntDesign } from "@expo/vector-icons";
+
+import { Wrapper, Header, Balance, BalanceContainer, BalanceTitle, Container } from "./styles";
+
+import  Suggestions  from "../../components/Suggestions";
+
+export default function Home(){
+    return (
+       <Wrapper>
+           <Container>
+            <Header>
+                <MaterialCommunityIcons name="qrcode-scan" size={30} color="#10c86e" />
+                <BalanceContainer>
+                    <BalanceTitle>Meu saldo</BalanceTitle>
+                    <Balance>R$ 10.000,00</Balance>
+                </BalanceContainer>
+                <AntDesign name="gift" size={30} color="#10c86e" />
+            </Header>
+
+            <Suggestions />
+
+           </Container>
+       </Wrapper> 
+    );
+}
